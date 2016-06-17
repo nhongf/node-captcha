4558c7690fada191547bf228d05ab22c4157bfa7
nhongf <nhon.huu.lai@saritasa.com>
2016-06-17 09:43:07 +0700

    Update captcha.js

--- captcha.js
+++ captcha.js
@@ -26,26 +26,26 @@ module.exports = function(params){
         ctx.strokeStyle = params.color;
         ctx.font = params.fontSize+'px sans';
 
-        for (var i = 0; i < 2; i++) {
-            ctx.moveTo(Math.floor(0.08*params.canvasWidth), Math.random() * params.canvasHeight);
-            ctx.bezierCurveTo(Math.floor(0.32*params.canvasWidth), Math.random() * params.canvasHeight, Math.floor(1.07*params.canvasHeight), Math.random() * params.canvasHeight, Math.floor(0.92*params.canvasWidth), Math.random() * params.canvasHeight);
-            ctx.stroke();
-        }
+        //for (var i = 0; i < 2; i++) {
+        //    ctx.moveTo(Math.floor(0.08*params.canvasWidth), Math.random() * params.canvasHeight);
+        //    ctx.bezierCurveTo(Math.floor(0.32*params.canvasWidth), Math.random() * params.canvasHeight, Math.floor(1.07*params.canvasHeight), Math.random() * params.canvasHeight, Math.floor(0.92*params.canvasWidth), Math.random() * params.canvasHeight);
+        //    ctx.stroke();
+        //}
 
-                var text = params.text || ('' + Math.random()).substr(3, params.codeLength);
+		var text = params.text || ('' + Math.random()).substr(3, params.codeLength);
 
-      for (i = 0; i < text.length; i++) {
-                        ctx.setTransform(Math.random() * 0.5 + 1, Math.random() * 0.4, Math.random() * 0.4, Math.random() * 0.5 + 1, Math.floor(0.375*params.fontSize) * i + Math.floor(0.25*params.fontSize), Math.floor(1.25*params.fontSize));
-                        ctx.fillText(text.charAt(i), 0, 0);
-                }
+      	for (i = 0; i < text.length; i++) {			
+			ctx.setTransform(Math.random() * 0.5 + 1, Math.random() * 0.4, Math.random() * 0.4, Math.random() * 0.5 + 1, Math.floor(1*params.fontSize) * i + Math.floor(0.5*params.fontSize), Math.floor(1.25*params.fontSize));
+			ctx.fillText(text.charAt(i), 0, 0);
+		}
 
-                canvas.toBuffer(function(err, buf) {
-            if(req.session)
-                req.session.captcha = text;
-            res.type('jpg');
-            res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-            res.header('Expires', 'Sun, 12 Jan 1986 12:00:00 GMT');
-            res.end(buf);
+		canvas.toBuffer(function(err, buf) {
+			if(req.session)
+				req.session.captcha = text;
+			res.type('jpg');
+			res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+			res.header('Expires', 'Sun, 12 Jan 1986 12:00:00 GMT');
+			res.end(buf);
         });
     };
 };
